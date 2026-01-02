96e49824edfab0e7d837424fe3f11199069c77b6
Bohdan259 <bodiiiiic@gmail.com>
2026-01-02 17:16:37 +0000

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,17 @@
+'use strict';
+const population = [
+    ...document.querySelectorAll('.population')
+];
+const total = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const sum = population.reduce((acc, el)=>{
+    const num = Number(el.textContent.replace(/,/g, ''));
+    return acc + (isNaN(num) ? 0 : num);
+}, 0);
+const average = Math.round(sum / population.length);
+if (total && averagePopulation) {
+    total.innerText = sum.toLocaleString();
+    averagePopulation.innerHTML = average.toLocaleString();
+}
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
